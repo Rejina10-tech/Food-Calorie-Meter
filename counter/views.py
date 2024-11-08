3000a3f1ef92e9e2861177d4014898eb9d570dc0
unknown <rezeena.dahal@gmail.com>
2024-11-08 14:29:05 +0400

    added API

--- counter/views.py
+++ counter/views.py
@@ -3,7 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def home(request):
+# KmDgeps/w+IBmZrDMzFy3g==FY9Re3n6fUNOZUYs
 
-    return render(request, 'home.html')
+def home(request):
+    import json
+    import requests
+    if request.method == 'POST':
+        query = request.POST['query']
+        api_url = 'https://api.api-ninjas.com/v1/nutrition?query='
+        api_request = requests.get(
+            api_url + query, headers={'X-Api-Key': 'KmDgeps/w+IBmZrDMzFy3g==FY9Re3n6fUNOZUYs'})
+        try:
+            api = json.loads(api_request.content)
+            print(api_request.content)
+        except Exception as e:
+            api = "oops! There was an error"
+            print(e)
+        return render(request, 'home.html', {'api': api})
+    else:
+        return render(request, 'home.html', {'query': 'Enter a valid query'})
 
